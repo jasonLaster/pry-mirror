891af26a489a402eb8b48746cba973cec7558652
Jason Laster <Jason.laster.11@gmail.com>
2012-08-20 17:48:11 -0400

    gridster ui stuff

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -21,7 +21,9 @@ $(function(){
       widget_margins: [10, 10],
       widget_base_dimensions: [width/20, width/20],
       min_cols: 6,
-      min_rows: 20
+      min_rows: 20,
+      max_size_x: 10,
+      max_size_y: 10
   }).data('gridster');
 
 gridster = $(".gridster ul").data('gridster')
